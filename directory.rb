140a782f22531f717487cc828c5927e142be11c4
Jennifer Bacon <jenniferbacon@Jennifers-MacBook-Pro.local>
2017-07-17 09:11:15 +0100

    Step8.4

--- directory.rb
+++ directory.rb
@@ -19,10 +19,10 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-    if student[:name].length < 12
-      puts "#{index+1}. #{student[:name]} (#{student[:cohort]}) cohort"
-    end
+  i = 0
+  while i < students.length
+    puts "#{students[i][:name]} (#{students[i][:cohort]}) cohort"
+    i +=1
   end
 end
 
