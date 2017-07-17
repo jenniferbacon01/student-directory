b931bc9fcd3e8d6c5fe764d12881131fefa2ef0d
Jennifer Bacon <jenniferbacon@Jennifers-MacBook-Pro.local>
2017-07-17 14:35:14 +0100

    step10 refactoring

--- directory.rb
+++ directory.rb
@@ -1,20 +1,23 @@
+
+@students = []
+
 def print_header
   puts "The students of Villains Academy".center(50)
   puts "-----------------".center(50)
 end
 
-def print(students)
-  if students.count <= 0
-    return
-  end
-  cohort_list = students.map do |a|
+def print_students
+   if @students.count <= 0
+     return
+   end
+  cohort_list = @students.map do |a|
     a[:cohort]
   end
   cohort_list.uniq!
   cohort_list.sort!
   cohort_list.each do |cohort|
     puts "cohort: #{cohort}"
-    students.each do |student|
+    @students.each do |student|
       if student[:cohort] == cohort
         puts student[:name]
       end
@@ -22,64 +25,66 @@ def print(students)
   end
 end
 
-def print_footer(students)
-  if students.count <= 0
+def print_footer
+  if @students.count <= 0
     return
   end
-  puts "Overall, we have #{students.count} great #{@s}".center(50)
+  puts "Overall, we have #{@students.count} great #{@s}".center(50)
 end
 
 def input_students
   puts "Please enter the names of the students".center(50)
   puts "To finish, just hit return twice".center(50)
-  students =[]
   name = gets.gsub(/\n/,"")
   puts "please enter which cohort he/she is in"
   cohort = gets.gsub(/\n/,"").to_sym
   while !name.empty? do
-    cohort ||= :november
-    students << {name: name, cohort: cohort}
-    if students.count == 1
+    #cohort ||= :november
+    @students << {name: name, cohort: cohort}
+    if @students.count == 1
       @s = "student"
     else
       @s = "students"
     end
-    puts "Now we have #{students.count} #{@s}".center(50)
+    puts "Now we have #{@students.count} #{@s}".center(50)
     cohort = ""
     name = gets.gsub(/\n/,"")
     puts "please enter which cohort he/she is in"
     cohort = gets.gsub(/\n/,"").to_sym
-    #cohort ||= :november
-
   end
-  students
 end
 
 def interactive_menu
-  students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-
-    selection = gets.chomp
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "Please enter one of the options above"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
 
+def show_students
+  print_header
+  print_students
+  print_footer
+end
 
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "Please enter one of the options above"
+  end
+end
 #nothing happens until we call the methods
 
 
