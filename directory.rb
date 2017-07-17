4264684fc16a2e23ae1980c1c42426184ae02c74
Jennifer Bacon <jenniferbacon@Jennifers-MacBook-Pro.local>
2017-07-17 16:35:30 +0100

    step11 saving to csv

--- directory.rb
+++ directory.rb
@@ -64,6 +64,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the students to a csv"
   puts "9. Exit"
 end
 
@@ -79,13 +80,25 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
     puts "Please enter one of the options above"
   end
 end
-#nothing happens until we call the methods
+
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
 
 
 interactive_menu
